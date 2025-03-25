daf1336f51086623540cb106b44511bd94bc86f3
Juston Morgan <justonmorgan@gmail.com>
2025-03-25 11:15:29 -0700

    Update Apollo client to use deployed GraphQL endpoint

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,6 +1,27 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink, from } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.error(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+
+  if (networkError) console.error(`[Network error]: ${networkError}`);
+});
+
+const httpLink = createHttpLink({
+  uri: 'http://scoring-engine.327165573193.us-west-2.amazonaws.com:8080/graphql',
+});
 
 export const apolloClient = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
 });
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ApolloProvider } from '@apollo/client';
-import { client } from './apollo';
+import { apolloClient } from './graphql/client';
 import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
+    <ApolloProvider client={apolloClient}>
       <App />
     </ApolloProvider>
   </React.StrictMode>,
